07d1d181e4229318225ae883aa1bac1189fa6db4
Simone Basso <bassosimone@gmail.com>
2017-10-23 04:32:04 +0200

    Filter exceptions and document the code

--- addon.cc
+++ addon.cc
@@ -1,4 +1,5 @@
-// Based off <https://github.com/nodejs/nan> examples (MIT license)
+// Based off <https://github.com/nodejs/nan> examples (MIT license).
+// Runs the HIRL MK test as a proof of concept.
 
 #include <measurement_kit/common.hpp>
 #include <measurement_kit/nettests.hpp>
@@ -7,37 +8,74 @@
 namespace mk {
 namespace node {
 
+// Generic worker that can be used to wrap all MK tests.
 class NettestWorker : public Nan::AsyncWorker {
  public:
+
+  // The NettestWorker constructor creates a generic nettest worker.
+  //
+  // The first argument is a callback, allocated with `new`, of which this
+  // object takes ownership. This callback will be called when the test
+  // is finished, with `null` as its unique argument.
+  //
+  // The second argument is the test to run. This is designed so that you
+  // can configure the test elsewhere and then pass it here when ready.
   NettestWorker(Nan::Callback *callback, mk::nettests::BaseTest test)
     : AsyncWorker{callback}, test{test} {}
 
   ~NettestWorker() {}
 
-  // Executed inside the worker-thread.
-  // It is not safe to access V8, or V8 data structures
-  // here, so everything we need for input and output
-  // should go on `this`.
+  // Executes the nettest inside the worker-thread.
+  //
+  // Note: it is not safe to access V8, or V8 data structures here, so
+  // everything we need for input and output should go on `this`.
+  //
+  // Note: here we filter exceptions for robustness. Node is compiled
+  // with exceptions disabled. My understanding is that it means the
+  // compiler has not generated any unwinding code. Better to stop the
+  // exception here as propagating it would be really unsafe.
+  //
+  // Note: depending on the version of MK, this would spawn another
+  // blocking thread that takes care of running the test. This is
+  // maybe a little inefficient, but it fits well with the C++ model
+  // that is enforced by Nan (i.e. that's the easiest way).
   void Execute() {
-    test.run();
+    try {
+      test.run();
+    } catch (...) {
+      // NOTHING
+    }
   }
 
-  // Executed when the async work is complete
-  // this function will be run inside the main event loop
-  // so it is safe to use V8 again
+  // Calls the final callback when the test is complete.
+  //
+  // Note: this function will be run inside the main event loop so it is
+  // safe to use V8 again.
   void HandleOKCallback() {
     Nan::HandleScope scope;
     v8::Local<v8::Value> argv[] = { Nan::Null() };
     callback->Call(1, argv);
   }
 
-private:
+ private:
   mk::nettests::BaseTest test;
 };
 
 } // namespace node
 } // namespace mk
 
+// The `run_hirl` method is what is called from Node to run the Http Invalid
+// Request Line (HIRL) test (i.e. the simpler MK test).
+//
+// Its only argument is the callback to be called when done.
+//
+// In real node bindings, this would probably a method of an exported
+// object used to configure and to start the test.
+//
+// We use Nan::AsyncQueueWorker, whose job is most likely to use libuv
+// threads to run a background task and keep the I/O loop running. I say
+// so because I noticed that, without using this abstraction, Node was
+// exiting before end of the test (when it was not segfaulting).
 NAN_METHOD(run_hirl) {
   Nan::Callback *callback = new Nan::Callback{info[0].As<v8::Function>()};
   Nan::AsyncQueueWorker(new mk::node::NettestWorker{
@@ -47,6 +85,10 @@ NAN_METHOD(run_hirl) {
   });
 }
 
+// The `Initalize` hook is called when the module is loaded.
+//
+// It basically only defines the `http_invalid_request_line` function to
+// map to the `run_hirl` real function defined above.
 NAN_MODULE_INIT(Initialize) {
   Nan::Set(
       target,
@@ -55,4 +97,5 @@ NAN_MODULE_INIT(Initialize) {
           Nan::New<v8::FunctionTemplate>(run_hirl)).ToLocalChecked());
 }
 
+// The `NODE_MODULE` macro tell node that this is a module.
 NODE_MODULE(addon, Initialize)
